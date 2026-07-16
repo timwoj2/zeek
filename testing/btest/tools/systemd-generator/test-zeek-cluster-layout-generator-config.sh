935582de0d4855f6d2983bee3f316f9a56ee12a7
Arne Welzel <arne.welzel@corelight.com>
2026-07-16 18:51:00 +0200

    fixup! tools/systemd-generator: Hack on zeek-cluster-layout-generator

--- testing/btest/tools/systemd-generator/test-zeek-cluster-layout-generator-config.sh
+++ testing/btest/tools/systemd-generator/test-zeek-cluster-layout-generator-config.sh
@@ -5,7 +5,8 @@
 # @TEST-EXEC: ${BUILD}/tools/systemd-generator/zeek-cluster-layout-generator -C $(pwd)/zeek.conf -o cluster-layout.zeek
 # @TEST-EXEC: zeek ./cluster-layout.zeek
 # @TEST-EXEC: CLUSTER_NODE=worker-1 zeek ./cluster-layout.zeek --parse-only
-# @TEST-EXEC: ZEEK_TELEMETRY_LISTEN_ADDRESS=[::1] CLUSTER_NODE=worker-1 zeek ./cluster-layout.zeek --parse-only
+# XXX This actually starts listening on the Prometheus endpoint.
+# XXX TEST-EXEC: ZEEK_TELEMETRY_LISTEN_ADDRESS=[::1] CLUSTER_NODE=worker-1 zeek ./cluster-layout.zeek --parse-only
 # @TEST-EXEC: btest-diff cluster-layout.zeek
 #
 # @TEST-EXEC: ${BUILD}/tools/systemd-generator/zeek-cluster-layout-generator -C $(pwd)/zeek-no-metrics.conf -o cluster-layout-no-metrics.zeek
